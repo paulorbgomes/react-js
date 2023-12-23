257e32ea7b79483ae9c6b59bdad3eed23608a6ee
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-12-23 10:09:18 -0300

    Aula004 - Aplicação em React JS

---
+++ react-devmedia/aula004/src/componentes/Footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return <footer>Desenvolvido com React</footer>;
+}
+
+export default Footer;
---
+++ react-devmedia/aula004/src/componentes/HomePage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "../Card/";
+import "./style.css";
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default HomePage;
