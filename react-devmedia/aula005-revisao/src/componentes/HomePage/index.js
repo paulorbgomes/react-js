5b9b43aaa3e312a9034e4bb75e49cbaa4f7cb3b5
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-12-23 16:01:31 -0300

    React JS - revisão

---
+++ react-devmedia/aula005-revisao/src/componentes/HomePage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import Card from "../Card/";
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default HomePage;
