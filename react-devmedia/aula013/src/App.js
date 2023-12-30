8d97efa57240d77cc4bf79374b6807a493d5e054
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-12-30 11:59:33 -0300

    React JS - aula013

---
+++ react-devmedia/aula013/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+import Welcome from "./componentes/Welcome/";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testando componente App ...</h1>
+      <Welcome nome="Paulo R. B. Gomes ..." />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-devmedia/aula013/src/componentes/Welcome/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Welcome(props) {
+  return <p>Bem-vindo, {props.nome}</p>;
+}
+
+export default Welcome;
