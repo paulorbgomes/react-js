9ce9aa8411aa8fac1f27d2a564a35888cc9321f8
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-03-30 15:47:44 -0300

    Atualização - Capítulo 01

---
+++ react-Mauricio_Maujor/cap01/aula001-variaveis.js
@@ -0,0 +1,7 @@
+var v1 = 10;
+const v2 = 20;
+{
+  let v3 = 30;
+  console.log(`${v1}, ${v2}, ${v3}`);
+}
+console.log(`${v1}, ${v2}`);
---
+++ react-Mauricio_Maujor/cap01/aula002-operador-ternario.js
@@ -0,0 +1,13 @@
+// let resultado = (condicao) ? (retorno se verdadeiro) : (retorno se falso)
+
+// if-else convencional
+let preco = 100;
+if (preco < 40) {
+  console.log("Livro barato!");
+} else {
+  console.log("Livro não é barato!");
+}
+
+// operador ternario
+let resposta = preco < 40 ? "Livro barato!" : "Livro não é barato!";
+console.log(resposta);
