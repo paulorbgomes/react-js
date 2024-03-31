f96fef1e16735ed2323dc72ed17dc26459534b4d
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-03-31 16:02:37 -0300

    React - atualização do cap. 01.

--- react-Mauricio_Maujor/cap01/aula005-map-filter-find.js
+++ react-Mauricio_Maujor/cap01/aula005-map-filter-find.js
@@ -1,4 +1,5 @@
 // Metodo map()
+/*
 const livros = ["CSS3", "HTML5", "JavaScript", "React", "PHP"];
 let livros1 = livros.map((livro) => "Livro: " + livro);
 let livros2 = livros.map((livro, index) => `Livro ${index}: ${livro}`);
@@ -6,6 +7,7 @@ let livros2 = livros.map((livro, index) => `Livro ${index}: ${livro}`);
 console.log(livros);
 console.log(livros1);
 console.log(livros2);
+
 
 const livrosA = [
   { titulo: "Construindo sites com HTML", autor: "Mauricio Samy Silva" },
@@ -23,8 +25,10 @@ console.log(livrosY);
 const valores = [10, 20, 40, 60, 80, 100];
 let valores2 = valores.map((valor) => 2 * valor);
 console.log(valores2);
+*/
 
 // Metodo filter()
+/*
 const numeros = [7, 14, 21, 40, 75, 100];
 let pares = numeros.filter((numero) => numero % 2 == 0);
 let impares = numeros.filter((numero) => numero % 2 != 0);
@@ -32,4 +36,26 @@ console.log(numeros);
 console.log(pares);
 console.log(impares);
 
+const livros = [
+  { titulo: "Construindo Sites com HTML", autor: "Mauricio Samy Silva" },
+  { titulo: "Web Scraping com Python", autor: "Ryan Mitchell" },
+  { titulo: "CSS3", autor: "Mauricio Samy Silva" },
+];
+
+let livros1 = livros.filter((livro) => livro.titulo === "CSS3");
+let livros2 = livros.filter((livro) => livro.autor === "Mauricio Samy Silva");
+let livros3 = livros.filter((livro) => livro.titulo.includes("com"));
+console.log(livros1);
+console.log(livros2);
+console.log(livros3);
+*/
+
 // Metodo find()
+const livros = [
+  { id: 1, titulo: "Construindo Sites com HTML" },
+  { id: 2, titulo: "Web Scraping com Python" },
+  { id: 3, titulo: "CSS3" },
+];
+let livros1 = livros.find((livro) => livro.id === 3);
+console.log(livros1);
+console.log(livros1.titulo);
