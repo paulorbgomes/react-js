4d3837b912be5f020b8b0300221f15e619ae0f44
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-03-31 08:33:11 -0300

    React - atualização cap. 01

--- react-Mauricio_Maujor/cap01/aula005-map-filter-find.js
+++ react-Mauricio_Maujor/cap01/aula005-map-filter-find.js
@@ -20,6 +20,16 @@ console.log(livrosA);
 console.log(livrosX);
 console.log(livrosY);
 
+const valores = [10, 20, 40, 60, 80, 100];
+let valores2 = valores.map((valor) => 2 * valor);
+console.log(valores2);
+
 // Metodo filter()
+const numeros = [7, 14, 21, 40, 75, 100];
+let pares = numeros.filter((numero) => numero % 2 == 0);
+let impares = numeros.filter((numero) => numero % 2 != 0);
+console.log(numeros);
+console.log(pares);
+console.log(impares);
 
 // Metodo find()
