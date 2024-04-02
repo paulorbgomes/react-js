9d75a6b9594b769c64ed13e0537892af927701e1
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-04-02 08:50:23 -0300

    React - atualização (cap 01).

---
+++ react-Mauricio_Maujor/cap01/aula009-sintaxe-jsx/exemplo01.js
@@ -0,0 +1,5 @@
+let elemento = document.createElement("h1");
+let titulo = document.createTextNode("Livro React do Maujor");
+const resultado = elemento.appendChild(titulo);
+
+console.log(resultado);
