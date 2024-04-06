98dc7bb9d5318eb4b4fceb26d00acce938bf9ca3
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-04-06 12:12:05 -0300

    React: atualização (cap. 03).

--- react-Mauricio_Maujor/livro-react-maujor/tabela-livros/src/App.js
+++ react-Mauricio_Maujor/livro-react-maujor/tabela-livros/src/App.js
@@ -1,9 +1,38 @@
-function App() {
-  return (
-    <div>
-      <h1>Tabela de livros</h1>
-    </div>
-  );
-}
+import React, {Component} from "react";
+import TabelaHead from "./components/TabelaHead";
+import TabelaBody from "./components/TabelaBody";
+import TabelaFoot from "./components/TabelaFoot";
+
+class App extends Component {
+  state = {
+    livros: [
+      {
+        id: "978-85-7522-632-2",
+        titulo: "CSS Grid Layout",
+        autor: "Maurício Samy Silva"
+      },
+      {
+        id: "978-85-7522-677-3",
+        titulo: "Node Essencial",
+        autor: "Ricardo L. Lecheta"
+      },
+      {
+        id: "978-85-7522-512-7",
+        titulo: "Aprendendo Material Design",
+        autor: "Kyle Mew"
+      }
+    ]
+  };
+
+  render() {
+    return (
+    <table className="tabela">
+      <TabelaHead/>
+      <TabelaBody livros = {this.state.livros}/>
+      <TabelaFoot/>
+    </table>
+    );
+  };
+};
 
 export default App;
