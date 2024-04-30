aae69169401a9f5e7a76bc6a65f64eca5e7d3f06
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-04-30 09:17:15 -0300

    React Aprenda Praticando: Cap. 02 (DOM Core).

---
+++ react-Mauricio_Maujor-NOVO/cap01/aula009-sintaxe-JSX/index.js
@@ -0,0 +1,11 @@
+let elemento = document.createElement("h1");
+let titulo = document.createTextNode("Livro React do Maujor");
+elemento.append(titulo);
+document.body.append(elemento);
+
+let resultado = elemento;
+console.log(resultado);
+
+// JSX
+//let resultadoJSX = <h1>Livro React do Maujor</h1>
+//console.log(resultadoJSX);
