b2ea05249700ed5296d37d87d48963404c378c18
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-05-10 07:41:19 -0300

    Ajustes iniciais

--- react-Mauricio_Maujor/cap01/aula001-variaveis.js
+++
@@ -1,7 +0,0 @@
-var v1 = 10;
-const v2 = 20;
-{
-  let v3 = 30;
-  console.log(`${v1}, ${v2}, ${v3}`);
-}
-console.log(`${v1}, ${v2}`);
--- react-Mauricio_Maujor/cap01/aula009-sintaxe-jsx/exemplo01.js
+++
@@ -1,5 +0,0 @@
-let elemento = document.createElement("h1");
-let titulo = document.createTextNode("Livro React do Maujor");
-const resultado = elemento.appendChild(titulo);
-
-console.log(resultado);
